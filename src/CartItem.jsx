b46b2d9cd3d3efea500f8f600a6f6a6610d9f2e3
harshmishra2209 <mishraharsh2209@gmail.com>
2025-09-27 15:00:44 -0400

    task 3 completed

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,53 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total =0;
+    cart.forEach(item =>
+    {
+        const costNumber = parseFloat(item.parse.substring(1));
+        total += costNumber * item.quantity;;
+    });
+    return total;
   };
-
+const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
   const handleContinueShopping = (e) => {
-   
+   e.preventDefault();
+   onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(uodateQuantity({
+        name: item.name,
+        quantity: item.quantity +1
+    }));
   };
 
   const handleDecrement = (item) => {
-   
-  };
-
+   if(item.quantity > 1)
+   {
+        dispatch(uodateQuantity({
+        name: item.name,
+        quantity: item.quantity - 1
+  }));
+   }
+   else
+   {
+    dispatch(removeItem(item.name));
+   }
+};
+  
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const costNumber = parseFloat(item.cost.substring(1));
+    return (costNumber * item.quantity).toFixed(2);
   };
 
   return (
